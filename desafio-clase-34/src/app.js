5809c6396819ee92a2d478ad03d342bd4e49bf37
SchmidtAlejo <schmidtalejo@gmail.com>
2024-06-03 16:30:15 -0300

    desafio clase 34

---
+++ desafio-clase-34/src/app.js
@@ -0,0 +1,84 @@
+import express from "express";
+import http from 'http';
+import ProductRouter from "./routes/product.router.js"
+import { Server } from 'socket.io'
+import CartRouter from "./routes/cart.router.js"
+import ViewsRouter from "./routes/views.router.js"
+import socketioRouter from "./routes/socketio.router.js"
+import sessionsRouter from "./routes/sessions.router.js"
+import mongoose from "mongoose"
+import handlebars from "express-handlebars";
+import session from "express-session";
+import cookieParser from 'cookie-parser'
+import passport from 'passport';
+import passportInit from './config/passport.config.js'
+import { config } from "./config/config.js";
+import { handleError } from "./middlewares/errors/index.js";
+import cors from 'cors'
+import { addLogger } from "./utils/logger.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const productRouter = new ProductRouter()
+const cartRouter = new CartRouter()
+const viewsRouter = new ViewsRouter()
+
+const PORT = config.SERVER_PORT
+
+const app = express()
+
+const httpServer = http.createServer(app);
+
+const socketServer = new Server(httpServer)
+
+app.engine('handlebars', handlebars.engine());
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(config.COOKIE_SECRET));
+app.use(addLogger)
+
+app.use(cors())
+
+passportInit()
+app.use(session(
+    {
+        secret: config.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: true,
+    }
+))
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.use("/api/products", productRouter.getRouter());
+app.use("/api/carts", cartRouter.getRouter());
+app.use("/api/sessions", sessionsRouter);
+app.use("/", viewsRouter.getRouter());
+
+const server = httpServer.listen(PORT, () => {
+    console.log(`Server OK in port ${PORT}`)
+})
+
+app.use('/socketio', socketioRouter(socketServer));
+
+const connect = async () => {
+    try {
+        await mongoose.connect(config.MONGO_URI, { dbName: config.DB_NAME })
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+app.use(handleError)
+
+connect()
---
+++ desafio-clase-34/src/controllers/products.controller.js
@@ -0,0 +1,86 @@
+import ProductService from "../services/products.service.js";
+import { generateProduct, generator } from "../utils/utils.js";
+
+class ProductsController {
+  static getProducts = async (req, res) => {
+    try {
+      const products = await ProductService.getProducts(
+        req.query.limit,
+        req.query.page,
+        req.query.category,
+        req.query.sort
+      );
+      const payload = {
+        status: "success",
+        payload: products.docs,
+        totalPages: products.totalPages,
+        prevPage: products.prevPage || null,
+        nextPage: products.nextPage || null,
+        page: products.page,
+        hasPrevPage: products.hasPrevPage,
+        hasPrevPage: products.hasNextPage,
+        prevLink: products.hasPrevPage ? `http://localhost:8080/api/products?page=${products.prevPage}` : null,
+        nextLink: products.hasNextPage ? `http://localhost:8080/api/products?page=${products.nextPage}` : null,
+      }
+      res.success(payload);
+    } catch (error) {
+      res.error500(error.message);
+    }
+  };
+
+  static getProductById = async (req, res) => {
+    try {
+      const product = await ProductService.getProductById(req.params.id);
+      res.success(product);
+    } catch (error) {
+      res.badRequest(error.message);
+    }
+  };
+
+  static test = async (req, res) => {
+    try {
+      await ProductService.test();
+      res.success({ "success": true });
+    } catch (error) {
+      res.badRequest(error.message);
+    }
+  };
+
+  static addProduct = async (req, res, next) => {
+    try {
+      const product = await ProductService.addProduct(req.body);
+      res.success(product);
+    } catch (error) {
+      next(error)
+    }
+  };
+
+  static updateProduct = async (req, res) => {
+    try {
+      const product = await ProductService.updateProduct(req.params.pid, req.body);
+      res.success(product);
+    } catch (error) {
+      res.badRequest(error.message);
+    }
+  };
+
+  static deleteProduct = async (req, res) => {
+    try {
+      await ProductService.deleteProduct(req.params.pid);
+      res.success({ success: true });
+    } catch (error) {
+      res.badRequest(error.message);
+    }
+  };
+
+  static generateProducts = async (req, res) => {
+    try {
+      const payload = generator(100, generateProduct());
+      res.success(payload);
+    } catch (error) {
+      res.badRequest(error.message);
+    }
+  }
+}
+
+export default ProductsController;
